a67ec7cb4b0d38518e72834daa614292aca649f9
keshariamudit24 <keshariamudit@gmail.com>
2026-05-13 17:28:55 +0530

    added the alias logic

---
+++ server/prisma/migrations/20260513111633_db_update/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `alias` on the `Url` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Url" DROP COLUMN "alias";
--- server/routes/shortenRoute.js
+++ server/routes/shortenRoute.js
@@ -6,15 +6,41 @@ const { PrismaClient } = prismaPkg
 const shortenRoute = express.Router()
 const client = new PrismaClient()
 
+async function lookup(key, value){
+    const url = await client.Url.findFirst({
+        where: {
+            key: value
+        }
+    })
+    if(url) return 1
+    return 0
+}
+
 
 shortenRoute.post('/', expressAsyncHandler(async (req, res) => {
-    const row = await client.Url.findFirst({
-            where: {
-                alias: true
-            }
+    const { longurl, alias } = req.body
+
+    if(lookup(longUrl, longurl)){
+        res.body.json({ msg: "url already exists, go to : " + url.shortUrl })
+    }
+
+    if(alias != ""){
+
+        if(lookup(shortUrl, alias)){
+            res.body.json({ msg: "url already exists, go to : " + url.shortUrl })
         }
-    )
-    return row
+
+        const newUrl = await client.Url.create({
+            data: {
+                longUrl: longurl,
+                shortUrl: alias
+            }
+        })
+        res.status(200).json({ msg: "url created successfully" })
+    }
+    else{
+        
+    }
 }))
 
 export default shortenRoute
